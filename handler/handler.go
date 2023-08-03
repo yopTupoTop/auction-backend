35efc0d410999fa218635a8a8322a7147487edd9
yopTupoTop <zlovnevremeny@yandex.ru>
2023-08-03 15:12:06 +0400

    add db logit to get handler func

--- handler/handler.go
+++ handler/handler.go
@@ -1,13 +1,31 @@
 package handler
 
 import (
+	"auction_backend/model"
 	"database/sql"
+	"log"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func GetHandler(c *fiber.Ctx, db *sql.DB) error {
-	return c.SendString("hello")
+	var asset model.PlacedAssets
+	var assets []model.PlacedAssets
+	rows, err := db.Query("SELECT * FROM placedAssets")
+	if err != nil {
+		log.Fatal(err)
+		c.JSON("An error occured")
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		rows.Scan(&asset)
+		assets = append(assets, asset)
+	}
+
+	return c.Render("asset", fiber.Map{
+		"PlacedAssets": assets,
+	})
 }
 
 func PostHandler(c *fiber.Ctx, db *sql.DB) error {
