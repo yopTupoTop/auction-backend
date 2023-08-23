86c1bef0b1c125c991ab90dfc20f97d8402892c7
yopTupoTop <zlovnevremeny@yandex.ru>
2023-08-23 16:54:54 +0400

    Test Handler: add simple test for postHandler

--- handler/handler_test.go
+++ handler/handler_test.go
@@ -1,10 +1,16 @@
 package handler
 
 import (
+	"auction_backend/event"
+	"auction_backend/model"
 	"database/sql"
+	"log"
 	"testing"
 
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/gofiber/fiber/v2"
+	_ "github.com/lib/pq"
+	"github.com/valyala/fasthttp"
 )
 
 func TestGetHandler(t *testing.T) {
@@ -29,6 +35,18 @@ func TestGetHandler(t *testing.T) {
 }
 
 func TestPostHandler(t *testing.T) {
+	app := fiber.New()
+	ctx := app.AcquireCtx(&fasthttp.RequestCtx{})
+
+	connStr := "postgresql://user:yoptupotop@localhost/auction?sslmode=disable"
+	dbase, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	newContract := model.ContractCreated{Auction: common.Address{0x0000000000000000000000000000000000000010}, Owner: common.Address{0x0000000000000000000000000000000000000001}}
+	event.ContractsCreated = append(event.ContractsCreated, newContract)
+
 	type args struct {
 		c  *fiber.Ctx
 		db *sql.DB
@@ -38,7 +56,15 @@ func TestPostHandler(t *testing.T) {
 		args    args
 		wantErr bool
 	}{
-		// TODO: Add test cases.
+
+		{
+			name: "first",
+			args: args{
+				c:  ctx,
+				db: dbase,
+			},
+			wantErr: false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
